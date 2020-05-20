b3675b615460107c4d3c7f4317ca92ab33ed65b0
Risto Lahtela <24460436+Rsl1122@users.noreply.github.com>
2020-05-20 14:22:27 +0300

    Made rendering of inventory & clue inventory dynamic

--- i18n/fi.js
+++ i18n/fi.js
@@ -1,8 +1,53 @@
 const i18n = {
+    "welcome": "Ratkaise ${nimi} murhan arvoitus SQL-kielellä",
+    "murder": "${nimi} on murhattu. Hänen rintaansa iskettiin veitsi eilen, kello 17 ja 19 välisenä aikana.\nViimeksi hänet nähtiin elossa hänen poistuessaan juhlistaan kesken kaiken tuntemattomasta syystä.\nHänet löydettiin pihalta kello 19:12 kuolleena pitelemässä laukkua. Laukku on tutkittavana.",
+    "mission": "Tehtäväsi on tutkia juhlien vieraiden osallisuutta murhaan, sekä etsiä vihjeitä.\nTätä varten sinulle on annettu Kyselin, perusohjeet sen käyttöä varten, sekä juhlien vieraslista.",
+    "similarity-disclaimer": "Kaikki yhtäläisyydet oikean maailman tapahtumiin ovat sattumaa. Henkilöt ja tapahtumat ovat keksittyjä.",
+    "inventory": "Tutkintamateriaali",
     "ok": "Selvä!",
     "clues-text": "Vihjeet",
+    "found-clues-text": "Löytämäsi vihjeet",
     "suspects-text": "Epäillyt",
-    "clue-001-hint": '"Tämän avulla taulusta voi valita sarakkeen."',
+    "clue-discover": "Löysit vihjeen!",
+    "clue-discover-info": "Löysit Ohjekortin:",
+    "clue-001-name": 'Kyselin-ohjekortti (Valinta)',
+    "clue-001-text": 'Ohjeet Kyselimen peruskäyttöön:\nSELECT * FROM Table; -- Hakee kaikki sarakkeet\nSELECT column FROM Table;\nSELECT column_1, column_2 FROM Table;',
+    "clue-001-hint": 'Näin taulusta valitaan sarakkeita.',
+    "clue-002-name": 'Kengänjäljet pihalla',
+    "clue-002-text": 'Jäljen tehneen kengän koko on selvästi 37.',
+    "clue-002-hint": 'Pihalla on useita kengänjälkiä samoista kengistä.',
+    "clue-003-name": 'Loki Silminnäkijän haastattelusta',
+    "clue-003-text": 'Poliisi: "Eli siis näitte kaksi tummaa hahmoa liikkumassa pihalla vähän ennen juhlia, osaisitteko kertoa tuntomerkkejä?"\n\nSilminnäkijä: "Toinen oli pitkä, ehkä 180-190cm ja toinen paljon lyhyempi, ehkä 155-165cm. Jompi kumpi tupakoi, näin tupakan kytevän maassa. Muuta en osaa sanoa."',
+    "clue-003-hint": 'Lokista löytyi tuntomerkkejä epäilyttävistä hahmoista..',
+    "clue-004-name": 'Kyselin-ohjekortti (Rajaus)',
+    "clue-004-text": "Ohjeet Kyselimen peruskäyttöön:\nSELECT x FROM Table WHERE x=42;\nSELECT nimi FROM Table WHERE nimi='Esimerkki';\n=, <, >, <=, >=, !=",
+    "clue-004-hint": 'Näin valinnasta rajataan rivejä',
+    "task-001-name": "Sähköinen vieraslista",
+    "task-001-description": "Emme ole ehtineet tallentaa kaikkea tietoa vieraskirjasta. Voisitko tulostaa meille kaikki vieraslistassa olevat tiedot Kyselimen avulla.",
+    "task-002-name": "Kerrotut vieraiden kenkien koot",
+    "task-002-description": "Vieraslistasta löytyy osa kenkien koosta, voisitko antaa meille listan nimistä ja kenkien koosta niin voimme käydä tutkimassa lisää.",
+    "task-003-name": "Mitatut vieraiden kenkien koot",
+    "task-003-description": "Löydetyn kengän jäljen koko oli 37. Mittaamaan puuttuvien vieraiden kenkien koot. Rajaa ne vieraat joiden kengät ovat sopivan kokoiset niin jututamme heitä lisää.",
+    "task-004-name": "Vieraiden pituudet",
+    "task-004-description": "Tarvitsemme listan vieraista, jotka sopivat tuntomerkkeihin. Valitse 180-190cm sekä 155-165cm pitkien vieraiden nimet ja pituudet.",
+    "task-005-name": "Tupakoivat vieraat",
+    "task-005-description": "Silminnäkijän mainitsemaa tupakantumppia ei ole vielä löytynyt. Tarvitsemme listan tupakoivien vieraiden nimistä.",
     "source-police": 'Poliisin tutkintamateriaalista',
-    "source-folder": 'Gustafsonin pitelemästä kansiosta'
+    "source-folder": '${nimi} pitelemästä laukusta',
+    "source-guests": 'Vieraslistasta',
+    get: function (key) {
+        return this[key.substr(5)];
+    },
 }
+
+replaceI18nContent = () => {
+    for (let key of Object.keys(i18n)) {
+        const newContent = i18n[key];
+        if (!(newContent instanceof Function)) {
+            for (let element of document.getElementsByClassName("i18n-" + key)) {
+                element.innerText = newContent;
+            }
+        }
+    }
+}
+replaceI18nContent();
--- js/main.js
+++ js/main.js
@@ -11,70 +11,233 @@ queryInputField.onblur = () => {
     }
 }
 
+const inventory = ['clue-001', 'task-001', 'clue-002', 'task-002', 'clue-003', 'clue-004', 'task-003', 'task-004', 'task-005'];
+const clueInventory = [];
+
 startGame = async () => {
     await hideElement("mission-info");
+    showInventory();
+}
+
+getItem = id => {
+    console.log(id)
+    if (id.includes("clue-")) {
+        return clues[id.substr(5)];
+    } else if (id.includes("task-")) {
+        return tasks[id.substr(5)];
+    }
+}
+
+function renderInventory() {
+    let render = '';
+    for (let id of inventory) {
+        let item = getItem(id);
+        render += item.item.type.render(item);
+    }
+    return render;
+}
+
+function renderInventoryIconLables() {
+    let render = '';
+    const sources = {};
+    for (let id of inventory) {
+        let from = getItem(id).item.from;
+        if (from) sources[from.icon] = i18n.get(from.text);
+    }
+    for (let key of Object.keys(sources)) {
+        render += `<p class="col-white"><i class="${key}"></i> ${sources[key]}</p>`
+    }
+    return render;
+}
+
+showInventory = () => {
     showElement("mission-select");
+    document.getElementById('inventory').innerHTML = renderInventory();
+    document.getElementById('inventory-labels').innerHTML = renderInventoryIconLables();
 }
 
 const itemSources = {
     police: {
-        icon: 'fa fa-balance',
+        icon: 'fa fa-balance-scale',
         text: 'i18n-source-police'
     },
     folder: {
         icon: 'fa fa-folder',
         text: 'i18n-source-folder'
+    },
+    guests: {
+        icon: 'fa fa-address-book',
+        text: 'i18n-source-guests'
     }
 }
 
 const smallItems = {
     clue: {
         render: function (clue) {
+            const from = clue.item.from ? `<i class="fa-fw ${clue.item.from.icon}" title="${i18n.get(clue.item.from.text)}"></i> ` : "";
+            return `<div class="item" id="${clue.item.id}" onclick="${clue.item.onclick}">
+                <div class="content clue">
+                    <p><br><br></p>
+                </div>
+                <p>${from}${i18n.get(clue.item.name)}</p>
+            </div>`;
         }
     },
     table: {
         render: function (table) {
+            const from = table.item.from ? `<i class="fa-fw fa-2x ${table.item.from.icon}" title="${i18n.get(table.item.from.text)}"></i> ` : "<i></i>";
+            return `<div class="item" id="${table.item.id}" onclick="${table.item.onclick}">
+                <div class="content table-paper">
+                    ${from}<br><br><br><br><br>
+                </div>
+                <p>${i18n.get(table.item.name)}</p>
+            </div>`;
         }
     },
     icon: {
         render: function (icon) {
+            const from = icon.item.from ? `<i class="fa-fw ${icon.item.from.icon}" title="${i18n.get(icon.item.from.text)}"></i> ` : "";
+            console.log(from)
+            return `<div class="item" id="${icon.item.id}" onclick="${icon.item.onclick}">
+                <i class="${icon.item.icon} fa-6x"></i>
+                <p>${from}${i18n.get(icon.item.name)}</p>
+            </div>`
         }
     }
 }
 
-const clues = {
-    "001": {
-        text: "SELECT column FROM table;",
-        hint: "i18n-clue-001-hint"
+function overwriteProperties(from, to) {
+    for (let key of Object.keys(from)) {
+        if (to[key] instanceof Array) {
+            to[key].push(...from[key]);
+        } else if (to[key] instanceof Object) {
+            to[key] = {...to[key], ...from[key]};
+        } else {
+            to[key] = from[key];
+        }
     }
 }
-const discoveredClues = [];
+
+createClue = (id, clue) => {
+    const base = {
+        item: {
+            id: `clue-${id}`,
+            name: `i18n-clue-${id}-name`,
+            onclick: `clue('${id}')`
+        },
+        header: "i18n-clue-discover",
+        text: `i18n-clue-${id}-text`,
+        hint: `i18n-clue-${id}-hint`,
+        unlocks: []
+    };
+    overwriteProperties(clue, base);
+    return base
+}
+
+createTask = (id, task) => {
+    const base = {
+        item: {
+            id: `task-${id}`,
+            name: `i18n-task-${id}-name`,
+            onclick: `task('${id}')`
+        },
+        sql: `task${id}.txt`,
+        description: `i18n-task-${id}-description`,
+        unlocks: []
+    };
+    overwriteProperties(task, base);
+    return base
+}
+
+const clues = {
+    "001": createClue('001', {
+        item: {type: smallItems.clue, from: itemSources.police},
+        header: "i18n-clue-discover-info"
+    }),
+    "002": createClue('002', {
+        item: {
+            type: smallItems.icon,
+            icon: 'fa fa-shoe-prints',
+            from: itemSources.police,
+        },
+        unlocks: ['task-002']
+    }),
+    "003": createClue('003', {
+        item: {
+            type: smallItems.icon,
+            icon: 'col-white fa fa-microphone-alt',
+            from: itemSources.police,
+        },
+        unlocks: ['clue-004', 'task-003', 'task-004', 'task-005']
+    }),
+    "004": createClue('004', {
+        item: {
+            type: smallItems.clue,
+            from: itemSources.police,
+        },
+        header: "i18n-clue-discover-info"
+    })
+}
 
 const tasks = {
-    "001": {
-        name: 'Vieraslista',
-        from: itemSources.police,
-        sql: 'task001.txt',
-        description: 'i18n-task-001'
+    "001": createTask('001', {
+        item: {
+            type: smallItems.icon,
+            icon: 'col-white fa fa-address-book',
+            from: itemSources.police,
+        },
+        unlocks: ['clue-002']
+    }),
+    "002": createTask('002', {
+        item: {
+            type: smallItems.table,
+            from: itemSources.police,
+        },
+        unlocks: ['clue-003']
+    }),
+    "003": createTask('003', {
+        item: {
+            type: smallItems.table,
+            from: itemSources.police,
+        }
+    }),
+    "004": createTask('004', {
+        item: {
+            type: smallItems.table,
+            from: itemSources.police,
+        }
+    }),
+    "005": createTask('005', {
+        item: {
+            type: smallItems.table,
+            from: itemSources.police,
+        }
+    })
+}
+
+function renderFoundClues() {
+    let render = '';
+    for (let clueID of clueInventory) {
+        const clue = clues[clueID];
+        render += `<div class="clue"><p>${i18n.get(clue.text).split('\n').join("<br>")}</p></div>`
     }
+    return render;
+}
+
+discoverClue = clueID => {
+    clueInventory.push(clueID);
+    document.getElementById('clue-list').innerHTML = renderFoundClues();
 }
 
 clueAnimation = clueID => {
     const clueBox = document.getElementById("clue-box");
-    if (!discoveredClues.length) {
-        clueBox.classList.remove("hidden");
-    }
+    if (!clueInventory.length) clueBox.classList.remove("hidden");
     let elementId = "clue-" + clueID;
-    let element = document.getElementById(elementId);
-    element.style.zIndex = '500'
-    element.style.position = 'absolute';
-    element.style.transition = "transform 0.5s";
-    element.style.transform = "scale(0, 0)";
-    delay(500).then(() => {
-        clueBox.style.fontSize = "3rem";
+    removeElement(elementId);
+    discoverClue(clueID);
+    return delay(500).then(() => {
+        clueBox.style.fontSize = "2rem";
         return delay(1000).then(() => {
-            removeElement(elementId)
-            discoveredClues.push(clueID);
             return shakeElement("clue-box")
                 .then(() => {
                     clueBox.style.fontSize = "";
@@ -84,8 +247,9 @@ clueAnimation = clueID => {
 }
 
 setupClueModal = (clue) => {
-    document.getElementById('clue-text').innerText = clue.text;
-    document.getElementById('clue-hint').innerText = i18n[clue.hint.substr(5)];
+    document.getElementById('clue-header').innerHTML = i18n.get(clue.header);
+    document.getElementById('clue-text').innerHTML = i18n.get(clue.text).split('\n').join('<br>');
+    document.getElementById('clue-hint').innerText = i18n.get(clue.hint);
 }
 
 clue = clueID => {
@@ -94,6 +258,10 @@ clue = clueID => {
     $('#clueModal').modal()
         .on('hidden.bs.modal', () => {
             clueAnimation(clueID);
+            for (let unlock of clue.unlocks) {
+                inventory.push(unlock);
+            }
+            $('#clueModal').off('hidden.bs.modal')
         });
 }
 
