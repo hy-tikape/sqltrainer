dae391a5473c751d7b2e34687b0413611d3ce7cb
Risto Lahtela <24460436+Rsl1122@users.noreply.github.com>
2020-06-10 16:07:36 +0300

    Combined information in skills.js and tasks.js to progression graph

---
+++ tasks/progression.js
@@ -0,0 +1,62 @@
+progression = [
+    {
+        id: "A",
+        tasks: ['001', '002', '003'],
+        requires: []
+    },
+    {
+        id: "B",
+        tasks: ['004', '005', '006', '007', '008'],
+        requires: ["A"]
+    },
+    {
+        id: "C",
+        tasks: ['010', '011', '009', '012'],
+        requires: ["B"]
+    },
+    {
+        id: "D",
+        tasks: ['013', '014'],
+        requires: ["B"]
+    },
+    {
+        id: "E",
+        tasks: ['015', '016', '017', '018', '019', '020', '021', '022'],
+        requires: ["C"]
+    },
+    {
+        id: "F",
+        tasks: ['023', '024', '025'],
+        requires: ["C"]
+    },
+    {
+        id: "G",
+        tasks: ['026', '027', '028', '029', '030', '031', '032', '033', '034'],
+        requires: ["D"]
+    },
+    {
+        id: "H",
+        tasks: ['035', '036', '037', '038'],
+        requires: ["D"]
+    },
+    {
+        id: "I",
+        tasks: [],
+        requires: ["F", "G"]
+    },
+    {
+        id: "J",
+        tasks: [],
+        requires: ["I"]
+    },
+    {
+        id: "K",
+        tasks: [],
+        requires: ["I"]
+    },
+    {
+        id: "L",
+        tasks: [],
+        requires: ["I"]
+    }
+]
