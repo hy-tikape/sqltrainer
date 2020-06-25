5fc2e721765745d594391f3e1fb8659bc74a1200
Risto Lahtela <24460436+Rsl1122@users.noreply.github.com>
2020-06-25 14:36:42 +0300

    Fix task completion when currentTask is null

--- js/tasks.js
+++ js/tasks.js
@@ -113,7 +113,7 @@ class Task extends ItemType {
         if (this.completed) return;
         const taskGroup = taskGroups.lookupTaskGroupWithTaskId(this.id);
         this.completed = true;
-        if (Views.TASK.currentTask.id === this.id) Views.TASK.updateTaskCompleteText();
+        if (Views.TASK.currentTask && Views.TASK.currentTask.id === this.id) Views.TASK.updateTaskCompleteText();
         inventory.update();
         await Views.INVENTORY.updateTaskGroup();
         const from = document.getElementById('query-run-button');
