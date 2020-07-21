ab024ee031b03ec33cb183fe3e355ce84141f13b
Risto Lahtela <24460436+Rsl1122@users.noreply.github.com>
2020-07-21 12:26:45 +0300

    Fixed title of books view

--- js/views.js
+++ js/views.js
@@ -337,6 +337,7 @@ class ReadBookView extends View {
                 <p>${i18n.get(item.shortName)}</p>
             </div>`
             }
+            document.getElementById("display-book-title").innerHTML = i18n.get("books-text");
             document.getElementById("display-book").innerHTML = render + '</div>';
         }
     }
