df4a83cca8f278737a1013a3a2abd465b3ce7cc6
Risto Lahtela <24460436+Rsl1122@users.noreply.github.com>
2020-07-30 15:56:59 +0300

    Fixed some accessibility issues with the task view

--- js/views.js
+++ js/views.js
@@ -254,9 +254,9 @@ class TaskView extends View {
         if (dropdown && previousAnswers.length) {
             let render = '';
             for (let answer of previousAnswers) {
-                render += `<a class="dropdown-item" role="listitem" href="javascript:void(0)" data-query="${answer.query}" onclick="Views.TASK.setQuery(event.target.dataset.query)">
+                render += `<button class="dropdown-item" role="option" data-query="${answer.query}" onclick="Views.TASK.setQuery(event.target.dataset.query)">
                     ${answer.correct ? `<i class="fa fa-fw fa-check col-green" aria-label="${i18n.get('correct')}"></i>` : `<i class="fa fa-fw fa-times col-red" aria-label="${i18n.get('incorrect')}"></i>`} ${answer.date}
-                </a>`
+                </button>`
             }
             dropdown.innerHTML = render;
             await showElementImmediately('previous-answers');
